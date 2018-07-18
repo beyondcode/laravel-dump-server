4b65b32979e76c38d73a4608475af5df6ca8f423
Max <maxmatteostaack@gmail.com>
2018-07-18 18:41:45 +0200

    cleanup config_path

--- helpers.php
+++ helpers.php
@@ -3,11 +3,13 @@ if (!function_exists('config_path')) {
     /**
      * Get the configuration path.
      *
+     * This is a polyfill for the missing shorthand function in lumen.
+     *
      * @param  string $path
      * @return string
      */
     function config_path($path = '')
     {
-        return app()->basePath() . DIRECTORY_SEPARATOR . 'config' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+        return app()->basePath('config') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
     }
 }
